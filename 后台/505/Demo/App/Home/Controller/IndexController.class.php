f1a83e6c58cecee5fa810f8962f74c2d4d2988da
blackcatrecycler <1121042683@qq.com>
2016-04-23 19:34:30 +0800

    最终目录调整
    应要求加一个外层文件夹，优化了内置链接，不管怎么放都不会出现404了
    详情观看后台Readme.md

--- 后台/505/Demo/App/Home/Controller/IndexController.class.php
+++ 后台/505/Demo/App/Home/Controller/IndexController.class.php
@@ -6,7 +6,7 @@ use Think\Controller;
 class IndexController extends Controller {
 	public function index() {
 		$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>', 'utf-8');
-		echo '<br/><a href ="/demo/index.php/home/index/hello" title="点我跳转">点我跳转</a>';
+		echo '<br/><a href ="' . U("index.php/home/Index/hello") . '" title="点我跳转">点我跳转</a>';
 	}
 
 	///显示demo///
@@ -19,7 +19,7 @@ class IndexController extends Controller {
 
 	}
 	public function _after_hello() {
-		echo 'Hello World<br/><a href="/demo/index.php/home/index/insert">点我添加数据</a>';
+		echo 'Hello World<br/><a href="' . U("index.php/home/Index/insert") . '">点我添加数据</a>';
 	}
 	///插入demo///
 
@@ -33,7 +33,7 @@ class IndexController extends Controller {
 			$admin->power = $_POST['power'] + 0;
 			$result = $admin->add();
 			if ($result) {
-				$this->success('新增成功', '/demo/index.php/home/index/hello');
+				$this->success('新增成功', U("index.php/home/Index/hello"));
 			} else {
 				$this->error('新增失败');
 			}
@@ -46,7 +46,7 @@ class IndexController extends Controller {
 		$admin = M('admins');
 		$result = $admin->delete($id);
 		if ($result) {
-			$this->success('删除成功', '/demo/index.php/home/index/hello');
+			$this->success('删除成功', U("index.php/home/Index/hello"));
 		} else {
 			$this->error('删除失败');
 		}
