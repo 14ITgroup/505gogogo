4261653d7201d13affa025c28703a512d18262fd
blackcatrecycler <1121042683@qq.com>
2016-04-23 16:28:01 +0800

    增删查改的demo
    demo，方便以后绑定

--- 后台/Application/Home/Controller/IndexController.class.php
+++ 后台/Application/Home/Controller/IndexController.class.php
@@ -8,22 +8,47 @@ class IndexController extends Controller {
 		$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>', 'utf-8');
 		echo '<br/><a href ="http://localhost/index.php/home/index/hello" title="点我跳转">点我跳转</a>';
 	}
+
+	///显示demo///
 	public function hello() {
 
 		$admin = M('admins');
 		$vo = $admin->select();
 		$this->assign("list", $vo);
 		$this->display();
+
 	}
 	public function _after_hello() {
 		echo 'Hello World<br/><a href="http://localhost/index.php/home/index/insert">点我添加数据</a>';
 	}
+	///插入demo///
+
 	public function insert() {
+		$this->display();
+		if (IS_POST) {
+			$admin = M('admins');
+			$admin->name = $_POST['name'];
+			$admin->account = $_POST['account'];
+			$admin->password = $_POST['password'];
+			$admin->power = $_POST['power'] + 0;
+			$result = $admin->add();
+			if ($result) {
+				$this->success('新增成功', '/index.php/home/index/hello');
+			} else {
+				$this->error('新增失败');
+			}
+
+		}
+	}
+	///删除demo///
+	public function delete() {
+		$id = I('request.id');
 		$admin = M('admins');
-		$admin->name = "blackcat";
-		$admin->account = "bk";
-		$admin->password = "bk";
-		$admin->power = 1;
-		$admin->add();
+		$result = $admin->delete($id);
+		if ($result) {
+			$this->success('删除成功', '/index.php/home/index/hello');
+		} else {
+			$this->error('删除失败');
+		}
 	}
 }
--- 后台/Application/Runtime/Cache/Home/5f1ee435d3f602d14be94a416f733bd3.php
+++ 后台/Application/Runtime/Cache/Home/5f1ee435d3f602d14be94a416f733bd3.php
@@ -2,15 +2,28 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<titl>数据展示</titl>
+	<title>数据展示</title>
+	<script type="text/javascript" src="/jquery-v1.10.2.min.js"></script>
+	</script>
+	<script type="text/javascript">
+		function checked(){
+			return false;
+		}
+	</script>
 </head>
-<body>
-<?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><li><?php echo ($i); ?></li>
-<li><?php echo ($vo["name"]); ?></li>
-<li><?php echo ($vo["account"]); ?></li>
-<li><?php echo ($vo["password"]); ?></li>
-<li><?php echo ($vo["power"]); ?></li>
-<br><?php endforeach; endif; else: echo "" ;endif; ?>
 
+<body>
+<form id="add" action="./delete"  method="post" >
+	序号 -- 名字 -- 账号 -- 密码 -- 权限<br>
+	<?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i; echo ($i); ?> --
+		<?php echo ($vo["name"]); ?> --
+		<?php echo ($vo["account"]); ?> --
+		<?php echo ($vo["password"]); ?> --
+		<?php echo ($vo["power"]); ?>
+		<a href="<?php echo U("index.php/home/Index/delete?id=$vo[id]");?>" >删除</a>
+	<br>
+	<br><?php endforeach; endif; else: echo "" ;endif; ?>
+	<input type="hidden" id="id" name='ID'  />
+</form>
 </body>
 </html>
