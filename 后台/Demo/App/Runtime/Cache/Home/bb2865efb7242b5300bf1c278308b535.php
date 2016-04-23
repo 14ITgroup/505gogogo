e706a6a97211d60541fe0a7fb10ffe48fce04ff1
blackcatrecycler <1121042683@qq.com>
2016-04-23 18:06:19 +0800

    正式调整目录
    使用方法：将后台目录下的所有文件统一放置在服务器根目录下。
    目录结构，见后台内的README.md

---
+++ 后台/Demo/App/Runtime/Cache/Home/bb2865efb7242b5300bf1c278308b535.php
@@ -0,0 +1,23 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>数据展示</title>
+
+</head>
+
+<body>
+<form id="add" action="./delete"  method="post" >
+	序号 -- 名字 -- 账号 -- 密码 -- 权限<br>
+	<?php if(is_array($list)): $i = 0; $__LIST__ = $list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i; echo ($i); ?> --
+		<?php echo ($vo["name"]); ?> --
+		<?php echo ($vo["account"]); ?> --
+		<?php echo ($vo["password"]); ?> --
+		<?php echo ($vo["power"]); ?>
+		<a href="<?php echo U("index.php/home/Index/delete?id=$vo[id]");?>" >删除</a>
+	<br>
+	<br><?php endforeach; endif; else: echo "" ;endif; ?>
+	<input type="hidden" id="id" name='ID'  />
+</form>
+</body>
+</html>
