2e1044fb0f8b5bc5a3df26d47e98157d23a96e96
ForeverYoungW <463671217@qq.com>
2016-04-24 17:22:23 +0800

    用户的登录注册功能
    更改数据库users表的state的默认值为1，用户注册时不用操作，如果管理员冻结改成0就可以。注册时只判断账户是否已经存在和两次密码是否一致。加密是简单的md5.邮箱和电话还未加入正则表达式验证

--- 后台/505/App/Home/Controller/IndexController.class.php
+++ 后台/505/App/Home/Controller/IndexController.class.php
@@ -7,6 +7,66 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+    	redirect(U('home/index/login'), 5, '页面跳转中。。。');
+        //$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
+
+    //http://localhost/505/index.php/home/Index/login.html
+    public function login(){
+    	$this->display();
+    	if(IS_POST){
+    		$user = M('users');
+    		$username=$_POST['username'];
+    		$pwd=$_POST['password'];
+    		//这里使用md5加密
+    		$password=md5($pwd);
+    		if($username==""||$password==""){
+    			echo "<script>alert('请输入用户名和密码！');history.go(-1);</script>";
+    		}else{
+    			$result=$user->where('account="%s" and password="%s"',$username,$password)->select();
+    			if($result){
+    				$this->success('登陆成功', U("index.php/home/Index/homepage"));
+    			}else{
+    				$this->error('登陆失败');
+    			}
+    		}
+    	}
+    }
+
+    public function _after_login() {
+		echo '<br/><a href="' . U("index.php/home/Index/register") . '">注册</a>';
+	}
+
+	public function register(){
+		$this->display();
+		if(IS_POST){
+			//判断是否两次输入的密码一致
+			if($_POST['password1']==$_POST['password2']){
+				$user=M('users');
+				$user->name=$_POST['name'];
+				$user->account=$_POST['account'];
+				//md5后存入数据库
+				$pwd=$_POST['password1'];
+				$user->password=md5($pwd);
+				$user->email=$_POST['email'];
+				$user->phonenumber=$_POST['phone'];
+				$user->address=$_POST['address'];
+				if($user->where('account="%s"',$user->account)->select()){
+					$this->error('您输入的账号已存在');
+				}
+				$result=$user->add();
+				if ($result) {
+				$this->success('注册成功', U("index.php/home/Index/login"));
+				} else {
+					$this->error('注册失败');
+				}
+			}else{
+				$this->error('两次输入的密码不一致');
+			}
+		}
+	}
+
+	public function homepage(){
+		$this->display();
+	}
 }
