7f657729f4a990d597d0cc11422e3e267d140096
RyLin <674167390@qq.com>
2016-04-25 09:08:41 +0800

    管理员管理页面功能实现
    只实现功能 未优化

--- 后台/505/Admin/App/Home/Controller/IndexController.class.php
+++ 后台/505/Admin/App/Home/Controller/IndexController.class.php
@@ -7,6 +7,116 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+ 		$this->display();
+    }	
+    public function notice()
+    {
+		$admin = M('admins');
+		$vo = $admin->select();
+		$this->assign("list", $vo);
+		$this->display();
+
     }
+ 	public function noticeedit()
+    {
+		// $id = I('request.id');
+		// $admin = M('admins');
+		// $vo = $admin->select();
+		// $this->assign("list", $vo);
+		$this->display();
+
+    }
+
+
+ 	public function users()
+    {
+ 		$admin = M('admins');
+		 $vo = $admin->select();
+		 $this->assign("list", $vo);
+		$this->display();
+
+    } 	
+    public function user()
+    {
+  //   	$id = I('request.id');
+		// $admin = M('admins');
+		// $vo = $admin->select();
+		// $this->assign("list", $vo);
+		$this->display();
+
+    }
+    public function admins()
+    {
+ 		$admin = M('admins');
+		$vo = $admin->select();
+		$this->assign("list", $vo);
+		$this->display();
+
+    } 	
+    public function admin()
+    {
+    	$id = I('request.id');
+		$admin = M('admins');
+		$vo = $admin->where('id='.$id)->select();
+		$this->assign("list", $vo);
+		$this->assign("id", $id);
+		$this->display();
+		if (IS_POST) {
+			$admin = M('admins');
+			$admin->id=$id;
+			$admin->name = $_POST['name'];
+			$admin->account = $_POST['account'];
+			$admin->password = $_POST['password'];
+			$admin->power = $_POST['power'] + 0;
+			$result = $admin->save();
+			if ($result) {
+				echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+				echo '<script type="text/javascript">alert("修改成功")</script>';
+				$this->success();
+			} else {
+				echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+				echo '<script type="text/javascript">alert("修改失败")</script>';
+			}
+
+		}
+		
+    }
+     public function addadmin()
+    {
+    	$this->display();
+			if (IS_POST) {
+				$admin = M('admins');
+				$admin->name = $_POST['name'];
+				$admin->account = $_POST['account'];
+				$admin->password = $_POST['password'];
+				$admin->power = $_POST['power'] + 0;
+				$result = $admin->add();
+				if ($result) {
+					echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+					echo '<script type="text/javascript">alert("新增成功")</script>';
+					$this->success();
+				} 
+				else {
+					echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+					echo '<script type="text/javascript">alert("新增失败")</script>';
+				}
+
+			}
+	}
+
+
+	///删除demo///
+	public function delete() {
+		$id = I('request.id');
+		$admin = M('admins');
+		$result = $admin->delete($id);
+		if ($result) {
+			echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+			echo '<script type="text/javascript">alert("删除成功")</script>';
+			$this->success();
+		} else {
+			echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+			echo '<script type="text/javascript">alert("删除失败")</script>';
+		}
+	}
 }
